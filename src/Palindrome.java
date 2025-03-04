9a969133ab8f6666a2ec3a1a9930f1d86f595df5
Kristopher Horton <khorton@miracosta.edu>
2025-03-03 16:28:41 -0800

    Initial Commit

---
+++ src/Palindrome.java
@@ -0,0 +1,17 @@
+public class Palindrome {
+    /**
+     * Break a stack into two, push the first half to one stack, then push
+     * second half to a stack, then pop & push to a second stack.
+     * If the string is odd, the middle element goes to both stacks (or neither!)
+     * R A C E C A R
+     * Stack 1: R A C E
+     * Middle Letter: E
+     * Stack 2: E C A R
+     * Re-stack either the first or second stack and compare them.
+     * @param s a string to test
+     * @return true if they are the same
+     */
+    public boolean isPalindrome(String s) {
+        return true;
+    }
+}
